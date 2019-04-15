7c13289f8657527bf942c3bdfdbe50dfab62fe86
zeevel <932690325@qq.com>
2019-04-15 20:16:02 +0800

    remove danger

--- src/main/java/com/zeevel/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/zeevel/o2o/web/shopadmin/ShopManagementController.java
@@ -43,11 +43,12 @@ public class ShopManagementController {
     @ResponseBody
     private Map<String,Object>registerShop(HttpServletRequest request){
         Map<String,Object>modelMap = new HashMap<String,Object>();
-        if(!CodeUtil.checkVerifyCode(request)){
-            modelMap.put("success",false);
-            modelMap.put("errMsg","输入了错误的验证码");
-            return modelMap;
-        }
+        //TODO
+//        if(!CodeUtil.checkVerifyCode(request)){
+//            modelMap.put("success",false);
+//            modelMap.put("errMsg","输入了错误的验证码");
+//            return modelMap;
+//        }
         //1.接受并转化相应的参数，包括店铺信息以及图片信息
         String shopStr = HttpServletRequestUtil.getString(request,"shopStr");
         ObjectMapper mapper = new ObjectMapper();
