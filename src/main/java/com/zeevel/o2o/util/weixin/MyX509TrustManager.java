c96015caa12da3c84293d25562de4c73ea8c2d70
zeevel <932690325@qq.com>
2019-04-17 15:00:50 +0800

    weixin auth2.0 complete

---
+++ src/main/java/com/zeevel/o2o/util/weixin/MyX509TrustManager.java
@@ -0,0 +1,25 @@
+package com.zeevel.o2o.util.weixin;
+
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * 证书信任管理器（用于https请求）
+ *
+ * @author xiangze
+ *
+ */
+public class MyX509TrustManager implements X509TrustManager {
+
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    }
+
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+    }
+
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
+}
+
